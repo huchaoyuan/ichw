c9028b4506ccd60c09f184018bc434170df3852a
胡朝元 <43382130+huchaoyuan@users.noreply.github.com>
2018-12-02 23:43:23 +0800

    Create currency.py

---
+++ pyassign2/currency.py
@@ -0,0 +1,51 @@
+"""currency.py: Exchange rate conversion
+
+__author__ = "HuChaoyuan"
+__pkuid__  = "1800011776"
+__email__  = "huchaoyuan@pku.edu.cn"
+"""
+from urllib.request import urlopen
+currency_from=input()
+currency_to=input()
+amount_from=input()
+def up(a):
+    '''Selective uppercase strings and converted into a dictionary.
+    a is the string to be selectively capitalized;
+    return is a string that is selectively capitalized
+    '''
+    b = ''
+    for i in range(len(a)):
+        if str(a[i:i+4])=='true'or str(a[i:i+5])=='false':
+            b=b+a[i].upper()
+        else:
+            b=b+a[i]
+    return b
+    pass
+def exchange(currency_from, currency_to, amount_from):
+    '''Export the desired exchange rate.
+    Parameter currency_from: the currency on hand.
+    Parameter currency_to: the currency to convert to.
+    Parameter amount_from: amount of currency to convert.
+    '''
+    doc=urlopen('http://cs1110.cs.cornell.edu/2016fa/a1server.php?from='+currency_from+'&to='+currency_to+'&amt='+amount_from)
+    docstr = doc.read()
+    doc.close()
+    jstr = docstr.decode('ascii')
+    result=eval(up(jstr))["to"].split()[0]
+    return(float(result))
+    pass
+def test_up():
+    '''test up()'''
+    assert(up('{ "from" : "2.5 United States Dollars", "to" : "2.1589225 Euros", "success" : true, "error" : "" }'))
+def test_exchange():
+    '''test exchange()'''
+    assert(exchange('USD','EUR','2.5'))
+def testAll():
+    """test all cases"""
+    test_up()
+    test_exchange()
+    print("All tests passed")
+def main():
+    print(exchange(currency_from, currency_to, amount_from))
+if __name__ == '__main__':
+    main()
